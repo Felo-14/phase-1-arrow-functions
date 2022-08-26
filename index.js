94c55cf75bad420a47f32e79522738caaa0cdf76
felix mwendwa <felix.mwendwa@student.moringaschool.com>
2022-08-26 08:56:39 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function(){
+    return 20;
+}
+divide(2000,100);
+
+const square = parameter=>parameter**parameter;
+let x = 2;
+square(2**2);
+
+const add = (parameter1,parameter2)=>parameter1+parameter2;
+let a = 3;
+let b = 4;
+add(a,b);
